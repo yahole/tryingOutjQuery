f521fa7fc857e4d231fb48499e54d814095c33fa
John Michael <111406110+yahole@users.noreply.github.com>
2024-01-04 20:50:56 -0800

    Add files via upload

---
+++ jQuery/index.js
@@ -0,0 +1,15 @@
+// $("h1").click(function() {
+//     $("h1").css("color", "purple")
+//     $("h1").addClass("biggy")
+// });
+
+
+
+// $("body").keypress(function(event){
+//     $("h1").html(event.key);
+// })
+
+$("body").on("mouseover", function() {
+    $("h1").css("color", "red");
+    
+})
